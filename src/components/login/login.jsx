ea27f2ec519f9f0682ffb8e75b284a88068baecd
Kwongiseok <ri2377@naver.com>
2021-01-09 22:38:46 +0900

    Route

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import styles from "./login.module.css";
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goToMaker = (userId) => {
+    history.push({
+      pathname: "/maker",
+      state: { id: userId },
+    });
+  };
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent)
-      .then(console.log);
+      .then((data) => goToMaker(data.user.uid));
   };
+  const onLogout = () => {
+    console.log("logout");
+    authService //
+      .logout()
+      .then(history.push("/"));
+  };
+  useEffect(() => {
+    authService //
+      .onAuthChange((user) => {
+        user && goToMaker(user.uid);
+      });
+  });
   return (
     <section className={styles.login}>
-      <Header />
+      <Header onLogout={onLogout} />
       <section>
         <h1 className={styles.title}>Login</h1>
         <ul className={styles.list}>
