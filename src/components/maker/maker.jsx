e750af2a1334acf301da65251c57d2f5ddf81bf6
Kwongiseok <ri2377@naver.com>
2021-01-10 21:01:28 +0900

    add CardAddForm form component

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -46,6 +46,9 @@ const Maker = ({ authService }) => {
     authService //
       .logout();
   };
+  const addCard = (card) => {
+    setCards([...cards, card]);
+  };
   useEffect(() => {
     authService.onAuthChange((user) => {
       if (!user) {
@@ -57,7 +60,7 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards} addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
