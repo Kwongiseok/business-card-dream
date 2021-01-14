dbd71392d4a2915a85a3cac5347157fc6dcdfa2b
Kwongiseok <ri2377@naver.com>
2021-01-14 11:56:25 +0900

    handle state change & update

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -4,12 +4,17 @@ import Card from "../card/card";
 import CardEditForm from "../card_edit_form/card_edit_form";
 import CardAddForm from "../card_add_form/card_add_form";
 
-const Editor = ({ cards, addCard }) => {
+const Editor = ({ cards, addCard, deleteCard, updateCard }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
-      {cards.map((card) => (
-        <CardEditForm key={card.id} card={card} />
+      {Object.keys(cards).map((key) => (
+        <CardEditForm
+          key={key}
+          card={cards[key]}
+          deleteCard={deleteCard}
+          updateCard={updateCard}
+        />
       ))}
       <CardAddForm onAdd={addCard} />
     </section>
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,8 +7,8 @@ import Preview from "../preview/preview";
 import styles from "./maker.module.css";
 const Maker = ({ authService }) => {
   const history = useHistory();
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: "1",
       name: "Giseok",
       company: "Kakao",
@@ -19,7 +19,7 @@ const Maker = ({ authService }) => {
       fileName: "Giseok",
       fileURL: null,
     },
-    {
+    2: {
       id: "2",
       name: "YJ",
       company: "Kakao",
@@ -30,7 +30,7 @@ const Maker = ({ authService }) => {
       fileName: "Giseok",
       fileURL: null,
     },
-    {
+    3: {
       id: "3",
       name: "YS",
       company: "Kakao",
@@ -41,14 +41,33 @@ const Maker = ({ authService }) => {
       fileName: "Giseok",
       fileURL: null,
     },
-  ]);
+  });
   const onLogout = () => {
     authService //
       .logout();
   };
-  const addCard = (card) => {
-    setCards([...cards, card]);
+  // const addCard = (card) => {
+  //   setCards([...cards, card]);
+  // };
+  const deleteCard = (card) => {
+    const updated = { ...cards };
+    delete updated[card.id];
+    setCards(updated);
   };
+  const createOrUpdateCard = (card) => {
+    // const updated = { ...cards };
+    // updated[card.id] = card;
+    // setCards(updated);
+    /* setState 콜백으로 사용할 수 있다. 이전 상태 값을 부른다. */
+    setCards((cards) => {
+      // setCards를 호출하기전 cards 값을 받아와서
+      const updated = { ...cards }; // 콜백 과정을 거친다.
+      updated[card.id] = card; // 새로 업데이트 된 애를 업데이트 해준다.
+      // 기존에 없던 id라면 오브젝트에 추가해주기 때문에 add의 기능을 포함하고 있다.
+      return;
+    });
+  };
+
   useEffect(() => {
     authService.onAuthChange((user) => {
       if (!user) {
@@ -60,7 +79,12 @@ const Maker = ({ authService }) => {
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+          updateCard={createOrUpdateCard}
+        />
         <Preview cards={cards} />
       </div>
       <Footer />
